a5ab1236b658402b96a7da8b097597dff69d51d4
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-08-28 19:52:20 +0200

    Added tests for posts homepage

--- pages/tests.py
+++ pages/tests.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 
 
 class PagesTests(SimpleTestCase):
+
     def test_home_status_code(self):
         response = self.client.get('/pages/')
         self.assertEqual(response.status_code, 200)
--- posts/tests.py
+++ posts/tests.py
@@ -1,12 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 
 class PostModelTest(TestCase):
 
     def setUp(self):
-        Post.objects.create(text='Mock text')
+        Post.objects.create(text='Stubbed text')
 
     def test_post_content(self):
         post = Post.objects.get(id=1)
-        self.assertEqual(f'{post.text}', 'Mock text')
+        self.assertEqual(f'{post.text}', 'Stubbed text')
+
+
+class PostsHomePageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='Stubbed text')
+
+    def test_WhenGetPosts_Then200(self):
+        self.assertEqual(self.client.get('/posts/').status_code, 200)
+
+    def test_WhenURLByReverse_Then200(self):
+        self.assertEqual(self.client.get(reverse('home')).status_code, 200)
+
+    def test_WhenURLByReverse_ThenUsePostsHomeTemplate(self):
+        self.assertTemplateUsed(
+            self.client.get(reverse('home')),
+            'posts/home.html'
+        )
