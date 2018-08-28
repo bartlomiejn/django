114fe572f80d05a6385ed84f2af6eb419fb1b232
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-08-28 19:36:00 +0200

    Added tests for posts, updated tests for pages

--- pages/tests.py
+++ pages/tests.py
@@ -3,10 +3,10 @@ from django.test import SimpleTestCase
 
 class PagesTests(SimpleTestCase):
     def test_home_status_code(self):
-        response = self.client.get('/')
+        response = self.client.get('/pages/')
         self.assertEqual(response.status_code, 200)
 
 
     def test_about_status_code(self):
-        response = self.client.get('/about/')
+        response = self.client.get('/pages/about/')
         self.assertEqual(response.status_code, 200)
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='Mock text')
+
+    def test_post_content(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(f'{post.text}', 'Mock text')
