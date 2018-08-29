5f5447be7dad49c255cf702fb6db569845a0d344
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-08-29 16:17:26 +0200

    Added blog app with templates and a list view

--- pages_proj/settings.py
+++ pages_proj/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates/posts'),
+            os.path.join(BASE_DIR, 'templates/blog'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
